ef48a102b81b748dfcaca744614b56810876e7e7
CHOI <seoung9066@naver.com>
2025-04-18 17:45:41 +0900

    requestMapping 추가

--- react-test/spring-backend/src/main/java/com/sgg/application/user/controller/UserController.java
+++ react-test/spring-backend/src/main/java/com/sgg/application/user/controller/UserController.java
@@ -1,23 +1,24 @@
 package com.sgg.application.user.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sgg.application.user.service.UserService;
 
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
     
     @Autowired
     UserService userService;
 
-    @GetMapping("/api/user/userList")
+    @GetMapping("/userList")
     public Map<String, Object> userList(@RequestParam Map<String, String> map) throws Exception {
         Map<String, Object> result = new HashMap<>();
         result.put("list", userService.userList(map));
