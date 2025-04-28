bba178d42f61ad34a69a2de274b107370e7a592d
CHOI <seoung9066@naver.com>
2025-04-28 09:25:58 +0900

    a

--- react-test/client/src/components/NotFound.jsx
+++ react-test/client/src/components/NotFound.jsx
@@ -4,7 +4,7 @@ function NotFound(props) {
   
   return (
     <>
-      <RunawayButton toastRef={props.props.toastRef} msg={'잘못된 경로. 경로우대. 대머리.'} />
+      <RunawayButton msg={'잘못된 경로. 경로우대. 대머리.'} />
     </>
   );
 }
